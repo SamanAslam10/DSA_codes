5a3ca0feafebe29437e578083d6b7d5931442f09
Saman Aslam <samanaslam835@gmail.com>
2026-03-07 19:05:06 +0500

    Insertion of a node at the end of the list

---
+++ SingleLinkedList/InsertionAtEndofList.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *link;
+};
+void InsertAtEnd(struct Node *Head , int data);
+int main()
+{
+    struct Node *Head = NULL;
+    Head = (struct Node*)malloc(sizeof(struct Node));
+    Head -> data = 10;
+    Head -> link = NULL;
+
+    struct Node *Current = NULL;
+    Current = (struct Node *)malloc(sizeof(struct Node));
+    Current -> data = 20;
+    Current -> link = NULL;
+
+    Head->link = Current;
+
+    Current = (struct Node *)malloc(sizeof(struct Node));
+    Current -> data = 30;
+    Current -> link = NULL;
+
+    Head->link->link = Current;
+
+    Current = (struct Node *)malloc(sizeof(struct Node));
+    Current -> data = 40;
+    Current -> link = NULL;
+
+    Head->link->link->link = Current;
+
+    InsertAtEnd(Head , 50);
+    return 0;
+}
+void InsertAtEnd(struct Node *Head , int data)
+{
+    struct Node *temp = NULL;
+    temp = (struct Node *)malloc(sizeof(struct Node));
+    temp -> data = data;
+    temp -> link = NULL;
+
+    struct Node *ptr = NULL;
+    ptr = Head;
+    while(ptr!=NULL)
+    {
+        ptr = ptr -> link;
+    }
+    ptr->link = temp;
+}
