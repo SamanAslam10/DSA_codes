952fb0104b05984214223b9dc5774d5e56a633af
Saman Aslam <samanaslam835@gmail.com>
2026-03-14 20:20:43 +0500

    Insertion of a node at the end of the doubly linked list

---
+++ DoublyLinkedList/InsertionAtTheEnd.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct Node
+{
+    int data;
+    struct Node *prev;
+    struct Node *next;   
+};
+struct Node* AddAtEnd(struct Node *Head,int data);
+int main()
+{
+    struct Node *Head = (struct Node*)malloc(sizeof(struct Node));
+    Head -> data = 10;
+    Head -> prev = NULL;
+    Head -> next = NULL;
+
+    struct Node *Current1 = (struct Node*)malloc(sizeof(struct Node));
+    Current1 -> data = 20;
+    Current1-> prev = Head;
+    Current1 -> next = NULL;
+    Head -> next = Current1;
+
+    struct Node *Current2 = (struct Node*)malloc(sizeof(struct Node));
+    Current2 -> data = 30;
+    Current2 -> prev = Current1;
+    Current2 -> next = NULL;
+
+    Current1 -> next = Current2;
+    
+    Head = AddAtEnd(Head,40);
+
+    struct Node *temp = Head;
+    while(temp != NULL)
+    {
+        printf("%d,%s" , temp -> data ," ");
+        temp = temp -> next;
+    }
+}
+struct Node* AddAtEnd(struct Node *Head,int data)
+{
+    struct Node *ptr = (struct Node*)malloc(sizeof(struct Node));
+    ptr -> data = data;
+    ptr -> next = NULL;
+    ptr -> prev = NULL;
+
+    struct Node *temp = Head;
+    while(temp->next != NULL)
+    {
+        temp = temp -> next;
+    }
+    temp -> next = ptr;
+    ptr -> prev = temp;
+
+    return Head;
+}
