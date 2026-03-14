3be165e3b31148427dfa2e25669a059c272037f0
Saman Aslam <samanaslam835@gmail.com>
2026-03-14 22:11:57 +0500

    Deletion Of Last Node

---
+++ DoublyLinkedList/DeletionOfLastNode.c
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct Node
+{
+    int data;
+    struct Node *prev;
+    struct Node *next;
+};
+struct Node* AddAtEnd(struct Node *Head,int data);
+struct Node* AddAtBegining(struct Node *Head,int data);
+struct Node* DeletelastNode(struct Node *Head);
+int main()
+{
+    struct Node *Head = AddAtBegining(Head , 25);
+    Head = AddAtEnd(Head , 35);
+    Head = AddAtEnd(Head , 45);
+
+    Head = DeletelastNode(Head);
+    struct Node *temp = Head;
+    while(temp != NULL)
+    {
+        printf("%d,%s" , temp -> data , "  ");
+        temp = temp -> next;
+    }
+
+    return 0;
+}
+struct Node* DeletelastNode(struct Node *Head)
+{
+    struct Node *ptr = Head;
+    while(ptr-> next!= NULL)
+    {
+        ptr = ptr -> next;
+    }
+    ptr -> prev -> next = NULL;
+    free(ptr);
+    ptr = NULL;
+
+    return Head;
+}
+struct Node* AddAtBegining(struct Node *Head,int data)
+{
+    struct Node *ptr = (struct Node*)malloc(sizeof(struct Node));
+    ptr -> data = data;
+    ptr -> next = NULL;
+    ptr -> prev = NULL;
+
+    Head = ptr;
+    return Head;
+}
+struct Node* AddAtEnd(struct Node *Head,int data)
+{
+    struct Node *ptr = (struct Node*)malloc(sizeof(struct Node));
+    ptr -> data = data;
+    ptr -> next = NULL;
+    ptr -> prev = NULL;
+
+    struct Node *temp = Head;
+    while(temp->next != NULL)
+    {
+        temp = temp -> next;
+    }
+    temp -> next = ptr;
+    ptr -> prev = temp;
+
+    return Head;
+}
