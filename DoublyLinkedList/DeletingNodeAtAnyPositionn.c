8b89e355af55baf3a21c23aebd578d0391d9feed
Saman Aslam <samanaslam835@gmail.com>
2026-03-14 23:37:49 +0500

    deletion of a node at any position in a double linked list

---
+++ DoublyLinkedList/DeletingNodeAtAnyPositionn.c
@@ -0,0 +1,74 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct Node
+{
+    int data;
+    struct Node *prev;
+    struct Node *next;
+};
+struct Node* AddAtEnd(struct Node *Head,int data);
+struct Node* AddAtBegining(struct Node *Head,int data);
+struct Node* DeleteAtAnyPosition(struct Node *Head , int position);
+int main()
+{
+    struct Node *Head; 
+    Head = AddAtBegining(Head , 25);
+    Head = AddAtEnd(Head , 35);
+    Head = AddAtEnd(Head , 45);
+
+    Head = DeleteAtAnyPosition(Head,2);
+    struct Node *temp = Head;
+    while(temp != NULL)
+    {
+        printf("%d,%s" , temp -> data , "  ");
+        temp = temp -> next;
+    }
+
+    return 0;
+}
+struct Node* AddAtBegining(struct Node *Head,int data)
+{
+    struct Node *ptr = (struct Node*)malloc(sizeof(struct Node));
+    ptr -> data = data;
+    ptr -> next = NULL;
+    ptr -> prev = NULL;
+
+    Head = ptr;
+    return Head;
+}
+struct Node* AddAtEnd(struct Node *Head,int data)
+{
+    struct Node *ptr = (struct Node*)malloc(sizeof(struct Node));
+    ptr -> data = data;
+    ptr -> next = NULL;
+    ptr -> prev = NULL;
+
+    struct Node *temp = Head;
+    while(temp->next != NULL)
+    {
+        temp = temp -> next;
+    }
+    temp -> next = ptr;
+    ptr -> prev = temp;
+
+    return Head;
+}
+struct Node* DeleteAtAnyPosition(struct Node *Head , int position)
+{
+    struct Node *ptr = Head;
+
+    while(position != 1)
+    {
+        ptr = ptr -> next;
+        position --;
+    }
+
+    struct Node *temp = ptr -> prev;
+    temp -> next = ptr -> next;
+    ptr -> next -> prev = temp;
+
+    free(ptr);
+    ptr = NULL;
+    return Head;
+}
