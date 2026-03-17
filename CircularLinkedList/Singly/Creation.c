75d4757557f5f1255b368ccbc2f6f4da3c4b155e
Saman Aslam <samanaslam835@gmail.com>
2026-03-17 22:58:34 +0500

    Addition of another node

--- CircularLinkedList/Singly/Creation.c
+++ CircularLinkedList/Singly/Creation.c
@@ -11,5 +11,11 @@ int main()
     struct Node *Head = (struct Node*)malloc(sizeof(struct Node));
     Head -> data = 10;
     Head -> next = Head;
-    
+
+    struct Node *Current = (struct Node*)malloc(sizeof(struct Node));
+    Current -> data = 20;
+    Current -> next = NULL;
+
+    Head -> next = Current;
+    Current -> next = Head;
 }
