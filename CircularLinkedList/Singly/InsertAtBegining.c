995867fd4f94f0fa453dcdef0671f24fa8a26d15
Saman Aslam <samanaslam835@gmail.com>
2026-03-18 12:30:16 +0500

    Initial Commit: Insertion in the begining of the circular linked list

---
+++ CircularLinkedList/Singly/InsertAtBegining.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+struct Node* Create(int arr[] , int n);
+void Display(struct Node *head);
+void RecursionDisplay(struct Node *p,struct Node *head);
+int main()
+{
+    int n = 5;
+    int array[] = {1,2,3,4,5};
+
+    struct Node *Head = Create(array , n);
+    Display(Head);
+    struct Node *p = Head;
+    RecursionDisplay( p , Head);
+}
+struct Node* Create(int arr[] , int n)
+{
+    struct Node *ptr , *last;
+    struct Node *Head = (struct Node*)malloc(sizeof(struct Node));
+    Head -> data = arr[0];
+    Head -> next = Head;
+    last = Head;
+
+    for(int i = 1; i < n ; i ++)
+    {
+        ptr = (struct Node*)malloc(sizeof(struct Node));
+        ptr -> data = arr[i];
+        ptr -> next = last ->next;
+        last -> next = ptr;
+        last = ptr;
+    }
+
+    return Head;
+}
+void Display(struct Node *head)
+{
+    struct Node *ptr = head;
+    do
+    {
+        printf("%d,%s" , ptr -> data , " ");
+        ptr = ptr -> next;
+    }
+    while(ptr != head);
+}
