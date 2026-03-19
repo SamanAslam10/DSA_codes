afe3f14c8697b456683337ffcc3bef99a692a387
Saman Aslam <samanaslam835@gmail.com>
2026-03-20 04:00:50 +0500

    Creation of function to count elements

--- CircularLinkedList/Singly/CoutingElements.c
+++ CircularLinkedList/Singly/CoutingElements.c
@@ -6,18 +6,6 @@ struct Node
     int data;
     struct Node *next;
 };
-struct Node* Create(int arr[] , int n);
-void Display(struct Node *head);
-int main()
-{
-    int n = 5;
-    int array[] = {1,2,3,4,5};
-
-    struct Node *Head = Create(array , n);
-    Display(Head);
-    struct Node *p = Head;
-    RecursionDisplay( p , Head);
-}
 struct Node* Create(int arr[] , int n)
 {
     struct Node *ptr , *last;
@@ -46,4 +34,26 @@ void Display(struct Node *head)
         ptr = ptr -> next;
     }
     while(ptr != head);
+}
+int CountElements(struct Node *tail)
+{
+    struct Node *ptr = tail;
+    int count = 0;
+
+    do
+    {
+        count ++;
+        ptr = ptr ->next;
+    }while(ptr != tail);
+
+    return count;
+}
+int main()
+{
+    int n = 5;
+    int array[] = {1,2,3,4,5};
+
+    struct Node *Tail = Create(array , n);
+    int count = CountElements(Tail);
+    printf( "%s%d","Total elements : " , count);
 }
