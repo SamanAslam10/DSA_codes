ad10c3d791cd608101425ee390ca00b879e911ff
Saman Aslam <samanaslam835@gmail.com>
2026-03-22 18:59:34 +0500

    Insertion of a node at the end of a doubly linked list

---
+++ CircularLinkedList/Doubly/InsertionAtEnd.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    struct Node *prev;
+};
+struct Node* Create(int arr[],int n)
+{
+    struct Node *ptr , *temp , *head;
+    head = (struct Node*)malloc(sizeof(struct Node));
+    head -> data = arr[0];
+    head -> prev = head;
+    head -> next = head;
+
+    temp = head;
+
+    for(int i = 1; i < n ; i ++)
+    {
+        ptr = (struct Node*)malloc(sizeof(struct Node));
+        ptr -> data = arr[i];
+        ptr -> next = head;
+        ptr -> prev = temp;
+
+        temp -> next = ptr;
+        temp = ptr;
+    }
+    return temp;
+}
+void Display(struct Node *tail)
+{
+    struct Node *ptr = tail->next;
+    do
+    {
+        printf("%d,%s" , ptr -> data , " ");
+        ptr = ptr -> next;
+    }
+    while(ptr->prev != tail);
+}
+struct Node* InsertAtEnd(struct Node* tail, int data)
+{
+    struct Node *ptr = (struct Node*)malloc(sizeof(struct Node*));
+    ptr -> data = data;
+    ptr -> next = ptr;
+    ptr -> prev = ptr; 
+
+    ptr -> prev = tail;
+    ptr -> next = tail -> next;
+
+    tail -> next -> prev = ptr; 
+    tail -> next = ptr;
+    tail = ptr;
+    return tail;
+}
+int main()
+{
+    int arr[] = {1,2,3,4,5,6};
+    int n = 6;
+
+    struct Node* tail = Create(arr,n);
+
+    tail = InsertAtEnd(tail,7);
+    Display(tail);
+}
