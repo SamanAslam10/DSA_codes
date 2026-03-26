d71c1f4762b7b45fa0f287d67848401c3e4b33ef
Saman Aslam <samanaslam835@gmail.com>
2026-03-26 22:08:17 +0500

    Implementation of Queue by Linked List

---
+++ Queue/Practice/ImplementQueueByLinkedList.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}*front = NULL,*rear = NULL;
+
+void Create(int data)
+{
+    struct Node* ptr = (struct Node*)malloc(sizeof(struct Node));
+    ptr -> data = data;
+    ptr -> next = NULL;
+
+    front = ptr;
+    rear = ptr;
+}
+void EnQueue(int data)
+{
+    struct Node* ptr = (struct Node*)malloc(sizeof(struct Node));
+    ptr -> data = data;
+    ptr -> next = NULL;
+
+    rear -> next = ptr;
+    rear = ptr;
+}
+void DeQueue()
+{
+    struct Node* ptr = front;
+    front = front -> next;
+    free(ptr);
+    ptr = NULL;
+}
+int IsEmpty()
+{
+    if(front == NULL && rear == NULL)
+    {
+        printf("%s" , "The queue is empty!");
+        return 1;
+    }
+    return 0;
+}
+int IsFull()
+{
+    struct Node* ptr = (struct Node*)malloc(sizeof(struct Node));
+    if(ptr == NULL)
+    {
+        printf("%s" , "The queue is full!");
+        return 1;
+    }
+    return 0;
+}
+void Display()
+{
+    struct Node *ptr = front;
+    while(ptr != NULL)
+    {
+        printf("%d", ptr -> data);
+        printf("\n");
+        ptr = ptr -> next;
+    }
+}
+int main()
+{
+    Create(10);
+    EnQueue(40);
+    EnQueue(30);
+    EnQueue(80);
+
+    Display();
+
+    DeQueue();
+    DeQueue();
+    EnQueue(50);
+
+    Display();
+    printf("%d" , IsEmpty());
+    printf("%d" , IsFull());
+    return 0;
+}
