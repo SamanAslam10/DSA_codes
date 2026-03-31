db0a25cc0e8282b181bce447897ae58902758ae5
Saman Aslam <samanaslam835@gmail.com>
2026-03-31 22:42:22 +0500

    BFS traversal

--- Queue/Assignment/BFSTraversal.cpp
+++ Queue/Assignment/BFSTraversal.cpp
@@ -3,13 +3,34 @@
 
 using namespace std;
 
-void BFStraversal()
+#define V 5
+void BFStraversal(int graph[V][V], int start)
 {
+    bool visited[V] = {false};
+    queue<int> q ;
 
+    visited[start] = true;
+    q.push(start);
+
+    while(!q.empty())
+    {
+        int node = q.front();
+        q.pop();
+
+        cout<<node<<" ";
+
+        for(int i = 0 ; i < V ; i ++)
+        {
+            if(graph[node][i] == 1 && !visited[i])
+            {
+                visited[i] = true;
+                q.push(i);
+            }
+        }
+    }
 }
 int main()
 {   
-    int V = 5;
     int graph[5][5] = 
     {
         {0, 1, 1, 0, 0}, 
@@ -18,5 +39,7 @@ int main()
         {0, 1, 0, 0, 0}, 
         {0, 0, 1, 0, 0}  
     }; 
+    int start = 0;
+    BFStraversal(graph , start );
     return 0;
 }
