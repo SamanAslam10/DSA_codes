fd0eae642bf349fd5113fd0b7430f885316f41d6
Saman Aslam <samanaslam835@gmail.com>
2026-03-31 22:22:43 +0500

    BFS traversal

---
+++ Queue/Assignment/BFSTraversal.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+void BFStraversal()
+{
+
+}
+int main()
+{   
+    int V = 5;
+    int graph[5][5] = 
+    {
+        {0, 1, 1, 0, 0}, 
+        {1, 0, 0, 1, 0}, 
+        {1, 0, 0, 0, 1}, 
+        {0, 1, 0, 0, 0}, 
+        {0, 0, 1, 0, 0}  
+    }; 
+    return 0;
+}
