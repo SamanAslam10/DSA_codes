25b5815fae81cc88ab4e7ade2d7dd00620854190
Saman Aslam <samanaslam835@gmail.com>
2026-04-01 09:30:58 +0500

    Implementation of Sliding window maximum using queue

---
+++ Queue/Assignment/SlidingWindowMaximum.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+void SlidingWindowMaximum(int i ,int arr[] , int k)
+{
+    queue<int> q;
+
+    for(i; i < k ; i ++ )
+    {
+        q.push(arr[i]);
+    }
+    int max = q.front();
+    q.pop();
+    for(int i = 0; i < k ; i ++ )
+    {
+        if(max < q.front() )
+        {
+            max = q.front();
+        }
+        q.pop();
+    }
+    cout<<max<<" ";
+    i ++;
+    SlidingWindowMaximum(i,arr , k);
+}
+int main()
+{
+    int k,n = 0;
+
+    cout<<"Enter the no. of elements you want to enter: ";
+    cin>>n;
+
+    int arrInput[n];
+
+    for(int i = 0 ; i < n ; i ++)
+    {
+        cout<<"Enter value : ";
+        cin>>arrInput[i];
+    }
+    cout<<"Enter the value of k : ";
+    cin>>k;
+
+    SlidingWindowMaximum(0,arrInput , k);
+
+    return 0;
+}
