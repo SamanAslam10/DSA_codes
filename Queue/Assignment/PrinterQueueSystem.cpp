b9916a9a496475b053c3fc0046a5308719defa16
Saman Aslam <samanaslam835@gmail.com>
2026-04-01 12:31:18 +0500

    MVP:printer queue system

--- Queue/Assignment/PrinterQueueSystem.cpp
+++ Queue/Assignment/PrinterQueueSystem.cpp
@@ -3,7 +3,49 @@
 
 using namespace std;
 
+struct PrintJob
+{
+    int id;
+    string filname;
+    int pages;
+};
+
+queue<PrintJob> printQueue;
+void AddJob(PrintJob job)
+{
+    printQueue.push(job);
+}
+void ProcessJob()
+{
+    if(!printQueue.empty())
+    {
+        PrintJob currentJob = printQueue.front();
+        printQueue.pop();
+
+        cout<<"Printing : "<<currentJob.filname<<endl;
+    }
+}
+bool IsEmpty()
+{
+    return printQueue.empty();
+}
+void NextJob()
+{
+    if(!printQueue.empty())
+    {
+        PrintJob nextJob = printQueue.front();
+
+        cout<<"Next File : "<<nextJob.filname<<endl;
+    }
+}
 int main()
 {
-    
+    PrintJob job1 = PrintJob();
+    job1.id = 1;
+    job1.filname = "DSA_quiz";
+    job1.pages = 2;
+
+    AddJob(job1);
+    NextJob();
+    ProcessJob();
 }
