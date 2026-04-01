821c0959fce8317c9585df8cf62f9b5db0dbf282
Saman Aslam <samanaslam835@gmail.com>
2026-04-01 08:26:50 +0500

    Implementation of Queue using Circular Linked List

---
+++ Queue/Assignment/CircularLinkedListQueue.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}*front, *rear;
+
+void EnQueue(int data)
+{
+
+}
+int DeQueue(){}
+bool IsEmpty(){}
+int Front(){}
+int Last(){}
+void Display(){}
+int main()
+{
+
+    return 0;
+}
