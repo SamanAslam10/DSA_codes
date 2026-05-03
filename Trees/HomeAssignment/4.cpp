95bc32dcaecb5cbeeb0b9f63f171d65df96651e6
Saman Aslam <samanaslam835@gmail.com>
2026-05-03 22:19:29 +0500

    Searching a value in a binary tree

---
+++ Trees/HomeAssignment/4.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    
+    vector<Node*> childern;
+
+    Node(int value)
+    {
+        data = value;
+    }
+};
+
+bool SearchValue(Node* root,int value)
+{
+    if(root == NULL)
+    {
+        return false;
+    }
+    if(root->data == value)
+    {
+        return true;
+    }
+    for(Node* child : root->childern)
+    {
+        if(SearchValue(child,value))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+int main()
+{
+    Node *root = new Node(1);
+
+    Node* c1 = new Node(2);
+    Node* c2 = new Node(3);
+    Node* c3 = new Node(4);
+
+    root ->childern.push_back(c1);
+    root ->childern.push_back(c2);
+    root ->childern.push_back(c3);
+
+    Node* c5 = new Node(5);
+    Node* c6 = new Node(6);
+    c1->childern.push_back(c5);
+    c3->childern.push_back(c6);
+
+    int value = 0;
+    cout<<"Enter the Value you want to search: ";
+    cin>>value;
+
+    cout<<SearchValue(root,value);
+    
+
+    return 0;
+}
